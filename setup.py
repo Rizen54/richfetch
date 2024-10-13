167c9f6767d17706c9509e157f4e4aad6d112bef
Rizen54 <psatvik54@proton.me>
2024-10-13 13:05:57 +0530

    Made it a module

---
+++ setup.py
@@ -0,0 +1,32 @@
+import setuptools
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+requirements = []
+for line in open('requirements.txt').readlines():
+    req = line.strip()
+    if req.startswith('#') or req == '':
+        continue
+    requirements.append(req)
+
+
+setuptools.setup(
+    name='richfetch',
+    version='0.1.0',
+    description='A minimal and colorful fetch utility.',
+    author='Satvik Pandey (Rizen54)',
+    author_email='psatvik54@proton.me',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Rizen54/richfetch/",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.11',
+    install_requires=requirements,
+)
